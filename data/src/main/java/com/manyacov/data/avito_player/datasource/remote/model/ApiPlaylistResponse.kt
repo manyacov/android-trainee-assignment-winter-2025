dc2d812eeaa632197e91ed515356c347a023acff
Mia <mariamasha12345@gmail.com>
2025-02-14 17:33:14 +0300

    update remote models

--- data/src/main/java/com/manyacov/data/avito_player/datasource/remote/model/ApiPlaylistResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.manyacov.data.avito_player.datasource.remote.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ApiPlaylistResponse(
-    val tracks: TracksInfoDto,
-)
--- data/src/main/java/com/manyacov/data/avito_player/datasource/remote/model/ArtistDto.kt
+++ data/src/main/java/com/manyacov/data/avito_player/datasource/remote/model/ArtistDto.kt
@@ -6,13 +6,5 @@ import kotlinx.serialization.Serializable
 data class ArtistDto(
     val id: Int,
     val name: String,
-    val link: String,
-    val picture: String,
-    val pictureSmall: String,
-    val pictureMedium: String,
-    val pictureBig: String,
-    val pictureXl: String,
-    val radio: Boolean,
-    val tracklist: String,
     val type: String
 )
